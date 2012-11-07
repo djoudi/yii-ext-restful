a9f045e2241f32ed2be781cbbc6153f375415382
golovanov <golovanov@ub-golovanov>
2012-11-07 18:46:14 +0200

    Add data provider

--- actions/RestfulDataProviderAction.php
+++ actions/RestfulDataProviderAction.php
@@ -5,9 +5,47 @@
  * @copyright (c) 2012, Igor Golovanov
  * @package yii-ext-restful
  * @category actions
+ * 
+ * @property IDataProvider $dataProvider Data Provider.
  */
 class RestfulDataProviderAction extends RestfulAction
 {
+    /**
+     * Data provider.
+     *
+     * @var IDataProvider
+     */
+    private $_dataProvider;
+    
+    /**
+     * Gets the data provider.
+     * 
+     * @return IDataProvider
+     */
+    public function getDataProvider()
+    {
+        if(!$this->_dataProvider instanceof IDataProvider) {
+            if($this->_dataProvider instanceof Closure) {
+                $this->_dataProvider = $this->_dataProvider($this);
+            } else {
+                $this->_dataProvider = Yii::createComponent($this->_dataProvider);
+            }
+        }
+        return $this->_dataProvider;
+    }
+    
+    /**
+     * Sets the data provider.
+     * 
+     * @param IDataProvider|Closure|array|string $dataProvider
+     * @return RestfulDataProviderAction
+     */
+    public function setDataProvider($dataProvider)
+    {
+        $this->_dataProvider = $dataProvider;
+        return $this;
+    }
+    
     public function run()
     {
         
